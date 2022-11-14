7a81685cd2152ecff10b197810eda76f3d846edf
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-14 19:25:29 +0100

    Create friend component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-  <ul>
-    <li v-for="friend of friends" :key="friend.id">
+  <section>
+    <header>
+        <h2>My friends</h2>
+    </header>
+    <ul>
+    <!-- <li v-for="friend of friends" :key="friend.id">
         <h2>{{friend.name}}</h2>
         <button @click="toggleDetails">Show Details</button>
         <ul v-if="detailsAreVisible">
         <li><strong>Phone:</strong> {{friend.phone}}</li>
         <li><strong>Email:</strong> {{friend.email}}</li>
         </ul>
-    </li>
+    </li> -->
+    <friend-contact></friend-contact>
   </ul>
+  </section>
 </template>
 
 <script>
+import FriendContact from './components/FriendContact.vue';
 /**
  * Default export for this file when importing in ES-2015 sintax.
  */
 export default {
+  components: { FriendContact },
     data () {
         return {
             friends: [
-            { 
+                { 
                     id: 'manuel',
                     name: 'Manuel Lorenz',
                     phone: '0123 3532 123',
@@ -38,5 +46,69 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
 
 </style>
