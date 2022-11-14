1fe06216a0247e3d48dc24e4c0093c41f83c8136
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-14 19:04:01 +0100

    Simple list without style

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul>
+    <li v-for="friend of friends" :key="friend.id">
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{friend.phone}}</li>
+        <li><strong>Email:</strong> {{friend.email}}</li>
+        </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+/**
+ * Default export for this file when importing in ES-2015 sintax.
+ */
+export default {
+    data () {
+        return {
+            friends: [
+            { 
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 3532 123',
+                    email: 'manuel@localhost.com'
+                },
+                { 
+                    id: 'julie',
+                    name: 'Julie Jonez',
+                    phone: '0123 3532 124',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
